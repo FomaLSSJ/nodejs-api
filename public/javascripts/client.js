9928a694d7ab545477934406e2d9ca5e72c3f047
Vyacheslav Yudin <monsterfoma@gmail.com>
2015-12-16 13:17:50 +0500

    Edit files, add client edit method

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -52,6 +52,34 @@ function addArticle(title) {
     });
 }
 
+function editArticle(id, title, author, description, images) {
+    var url = 'http://localhost:1337/api/articles/' + id;
+
+    if (id == 'undefined' || id == null ||
+        title == 'undefined' || title == null ||
+        author == 'undefined' || author == null ||
+        description == 'undefined' || description == null ||
+        images == 'undefined' || images == null || $.type(images) != 'array' ) {
+        console.log({error: 'Empty field'});
+        return false;
+    }
+
+    $.ajax({
+       type:'PUT',
+        dataType:'json',
+        url:url,
+        data:{
+            title:title,
+            author:author,
+            description:description,
+            images:images
+        },
+        success:function(res) {
+            console.log(res);
+        }
+    });
+}
+
 function deleteArticle(id) {
     var url = 'http://localhost:1337/api/articles/' + id;
 
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
   res.render('index', { title: 'NodeAPI', using: 'NodeJS, Express, Jade, LESS' });
-=======
-  res.render('index', { title: 'Express' });
->>>>>>> 1b0567b2121745098f7fb7469c34e6165f3f04b9
 });
 
 module.exports = router;
