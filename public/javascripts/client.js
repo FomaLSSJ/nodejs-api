3bdc888f540f1ef2f0466558ea9c79b9d3507c2b
Vyacheslav Yudin <monsterfoma@gmail.com>
2015-12-16 16:19:14 +0500

    Add front-end

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,3 +1,83 @@
+$(document).ready(function() {
+    $('body').on('click', 'button#getArt', function() {
+        $('#advance').text('');
+        $('#advance').append(
+            '<button id="showArt">Show Article</button>' +
+            '<input id="id" type="text" placeholder="Input article id" />'
+        );
+    });
+
+    $('body').on('click', 'button#showArt', function() {
+        getArticle($('#id').val());
+    });
+
+    $('body').on('click', 'button#addArt', function() {
+        $('#advance').text('');
+        $('#advance').append(
+            '<button id="pushArt">Push Article</button>' +
+            '<input id="title" type="text" placeholder="Title" />' +
+            '<input id="author" type="text" placeholder="Author" />' +
+            '<input id="description" type="text" placeholder="Description" />' +
+            '<input id="thumbnail" type="text" placeholder="Thumbnail URL" />' +
+            '<input id="detail" type="text" placeholder="Detail URL" />'
+        );
+    });
+
+    $('body').on('click', 'button#pushArt', function() {
+        addArticle(
+            $('#title').val(),
+            $('#author').val(),
+            $('#description').val(),
+            [
+                {kind: 'thumbnail', url: $('#thumbnail').val()},
+                {kind: 'detail', url: $('#detail').val()}
+            ]
+        );
+    });
+
+    $('body').on('click', 'button#editArt', function() {
+        $('#advance').text('');
+        $('#advance').append(
+            '<button id="putArt">Put Article</button>' +
+            '<input id="id" type="text" placeholder="Id" />' +
+            '<input id="title" type="text" placeholder="Title" />' +
+            '<input id="author" type="text" placeholder="Author" />' +
+            '<input id="description" type="text" placeholder="Description" />' +
+            '<input id="thumbnail" type="text" placeholder="Thumbnail URL" />' +
+            '<input id="detail" type="text" placeholder="Detail URL" />'
+        );
+    });
+
+    $('body').on('click', 'button#putArt', function() {
+       editArticle(
+           $('#id').val(),
+           $('#title').val(),
+           $('#author').val(),
+           $('#description').val(),
+           [
+               {kind: 'thumbnail', url: $('#thumbnail').val()},
+               {kind: 'detail', url: $('#detail').val()}
+           ]
+       );
+    });
+
+    $('body').on('click', 'button#getArts', function() {
+        getArticles();
+    });
+
+    $('body').on('click', 'button#delArt', function() {
+        $('#advance').text('');
+        $('#advance').append(
+            '<button id="remArt">Remove Article</button>' +
+            '<input id="id" type="text" placeholder="Input article id" />'
+        );
+    });
+
+    $('body').on('click', 'button#remArt', function() {
+        deleteArticle($('#id').val());
+    });
+});
+
 function getArticle(id) {
     var url = 'http://localhost:1337/api/articles/' + id;
 
@@ -7,46 +87,52 @@ function getArticle(id) {
     }
 
     $.ajax({
-        type:'GET',
-        dataType:'json',
-        url:url,
-        success:function(req) {
-            console.log(req);
+        type: 'GET',
+        dataType: 'json',
+        url: url,
+        success: function(res) {
+            console.log(res);
+            $('#output').text('');
+            $('#output').append('TITLE: ' + res.article.title + '<br>' +
+                    'AUTHOR: ' + res.article.author + '<br>' +
+                    'DESCRIPTION: ' + res.article.description + '<br>'
+            );
+            $(res.article.images).each(function(i) {
+                $('#output').append('IMAGES ' +  res.article.images[i].kind.toUpperCase() + ':<br>' +
+                        '<img src="' + res.article.images[i].url + '"/><br>'
+                );
+            });
         }
     });
 }
 
 function getArticles() {
     $.ajax({
-        type:'GET',
-        dataType:'json',
-        url:'http://localhost:1337/api/articles',
-        success:function(req) {
-            console.log(req);
+        type: 'GET',
+        dataType: 'json',
+        url: 'http://localhost:1337/api/articles',
+        success: function(res) {
+            console.log(res);
+            $('#output').text('');
+            $.each(res, function(i) {
+                $('#output').append('ID: ' + res[i]._id + '</br>');
+            });
         }
     });
 }
 
-function addArticle(title) {
-    if (title == 'undefined' || title == '' || title == null) {
-        console.log({error:'Title empty'});
-        return false;
-    }
-
+function addArticle(title, author, description, images) {
     $.ajax({
-        type:'POST',
-        dataTyep:'json',
-        url:'http://localhost:1337/api/articles',
-        data:{
-            title: 'TestArticle',
-            author: 'John Doe',
-            description: 'lorem ipsum dolar sit amet',
-            images: [
-                {"kind":"thumbnail", "url":"http://habrahabr.ru/images/write-topic.png"},
-                {"kind":"detail", "url":"http://habrahabr.ru/images/write-topic.png"}
-            ]
+        type: 'POST',
+        dataTyep: 'json',
+        url: 'http://localhost:1337/api/articles',
+        data: {
+            title: title,
+            author: author,
+            description: description,
+            images: images
         },
-        success:function(res) {
+        success: function(res) {
             console.log(res);
         }
     });
@@ -55,26 +141,17 @@ function addArticle(title) {
 function editArticle(id, title, author, description, images) {
     var url = 'http://localhost:1337/api/articles/' + id;
 
-    if (id == 'undefined' || id == null ||
-        title == 'undefined' || title == null ||
-        author == 'undefined' || author == null ||
-        description == 'undefined' || description == null ||
-        images == 'undefined' || images == null || $.type(images) != 'array' ) {
-        console.log({error: 'Empty field'});
-        return false;
-    }
-
     $.ajax({
-       type:'PUT',
-        dataType:'json',
-        url:url,
-        data:{
-            title:title,
-            author:author,
-            description:description,
-            images:images
+       type: 'PUT',
+        dataType: 'json',
+        url: url,
+        data: {
+            title: title,
+            author: author,
+            description: description,
+            images: images
         },
-        success:function(res) {
+        success: function(res) {
             console.log(res);
         }
     });
@@ -83,17 +160,13 @@ function editArticle(id, title, author, description, images) {
 function deleteArticle(id) {
     var url = 'http://localhost:1337/api/articles/' + id;
 
-    if (id == 'undefined' || id == null) {
-        console.log({error: 'ID empty'});
-        return false;
-    }
-
     $.ajax({
-        type:'DELETE',
-        dataType:'json',
-        url:url,
-        success:function(req) {
-            console.log(req);
+        type: 'DELETE',
+        dataType: 'json',
+        url: url,
+        success: function(res) {
+            console.log(res);
+            $('#output').text('Article id: ' + id + ' removed');
         }
     });
 }
